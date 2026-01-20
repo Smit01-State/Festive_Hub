56997c8c92967f8c17868f03f6839663f10482bb
Smit01-State <113623006+Smit01-State@users.noreply.github.com>
2026-01-20 15:48:02 +0530

    create a sign up page

--- app/src/main/java/com/Final_year_App/android/CreateAccount.java
+++ app/src/main/java/com/Final_year_App/android/CreateAccount.java
@@ -1,21 +1,12 @@
 package com.Final_year_App.android;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.material.snackbar.Snackbar;
 
 public class CreateAccount extends AppCompatActivity {
 
